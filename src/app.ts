d4a5f60393bc12d7518bb08ded1d03f5ae18cbe0
Lucas Hijman <lucashijman12@gmail.com>
2022-11-24 14:34:48 +0100

    Revert "handle joystick (dis)connection"
    This reverts commit 4280aa0b9839a3c0ebf00bc4318b8367c528838f.

--- src/app.ts
+++ src/app.ts
@@ -7,8 +7,6 @@ import { Level3 } from "./scenes/level3"
 import { EndScene } from "./scenes/end-scene"
 import { UIScene } from "./scenes/ui-scene"
 import { WonScene } from "./scenes/won-scene"
-import { Arcade } from "./arcade/arcade";
-import { Player } from "./objects/player";
 
 const config: GameConfig = {
     width: 800,
@@ -30,33 +28,8 @@ const config: GameConfig = {
 };
 
 export class Game extends Phaser.Game {
-    arcade: Arcade
-    joystickListener: EventListener
-    player: Player
-
     constructor(config: GameConfig) {
         super(config)
-    }
-
-    doneLoading() {
-        this.arcade = new Arcade(this)
-
-        this.joystickListener = (e: Event) => this.joyStickConnected(e as CustomEvent)
-        document.addEventListener("joystickcreated", this.joystickListener)
-    }
-
-    joyStickConnected(e: CustomEvent) {
-        let joystick = this.arcade.Joysticks[e.detail]
-
-        for (const buttonEvent of joystick.ButtonEvents) {
-            document.addEventListener(buttonEvent, () => console.log(buttonEvent))
-        }
-
-        this.player = new Player(joystick)
-    }
-
-    public disconnect() {
-        document.removeEventListener("joystickcreated", this.joystickListener)
     }
 }
 
